5098b2f67f136dd2d8cd6da89f48690ed58a35a8
Abdul-Wahab-318 <wahabmaliq@gmail.com>
2021-05-20 23:57:10 +0500

    removes font awesome cdn for better loading speed

--- src/components/helpCenter/HelpCenter.js
+++ src/components/helpCenter/HelpCenter.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import './HelpCenter.css'
+import lifeRing from '../../assets/life-ring.svg'
+
+
 export default function HelpCenter() {
     return (
         <div className="help-center">
@@ -8,7 +11,7 @@ export default function HelpCenter() {
                     <div className="help-center-inner">
                         <h1>We are all close together</h1>
                         <p>A problem, a question, an emergency? <br /> Do not hesitate to visit the help center, <span>we can help you</span></p>
-                        <button><i className="far fa-life-ring"></i> Help center</button>
+                        <button><img src={lifeRing} alt="" /> Help center</button>
                     </div>
                 </div>
             </div>
